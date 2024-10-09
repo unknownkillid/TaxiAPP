dfc6c59c0c88b196fd2611589e24124a02c7a57c
Unknown <102482809+unknownkillid@users.noreply.github.com>
2024-10-09 14:15:36 +0400

    carousel optimized

--- script/script.js
+++ script/script.js
@@ -1,14 +1,19 @@
+const images = [];
+for (let i = 1; i <= 3; i++) {
+    images[i] = new Image();
+    images[i].src = `./assets/carousel/${i}.jpg`; 
+}
+
 const carouselContainer = document.getElementById('carouselContainer');
-let currentImageIndex = 1; 
+let currentImageIndex = 1;
 
 function carousel() {
     carouselContainer.style.backgroundImage = `url(./assets/carousel/${currentImageIndex}.jpg)`;
     currentImageIndex++;
-
     if (currentImageIndex > 3) {
         currentImageIndex = 1;
     }
 }
 
-setInterval(carousel, 7000);
+setInterval(carousel, 3000);
 carousel();
