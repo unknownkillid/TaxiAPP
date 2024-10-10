35e3995296e4b837b994ab1d0b966d35e69d0979
[R*] Dante <102482809+unknownkillid@users.noreply.github.com>
2024-10-10 21:18:22 +0400

    courier registration added

--- script/script.js
+++ script/script.js
@@ -1,18 +1,18 @@
 const images = [];
 for (let i = 1; i <= 3; i++) {
-    images[i] = new Image();
-    images[i].src = `./assets/carousel/${i}.jpg`;
+  images[i] = new Image();
+  images[i].src = `./assets/carousel/${i}.jpg`;
 }
 
 const carouselContainer = document.getElementById('carouselContainer');
 let currentImageIndex = 1;
 
 function carousel() {
-    carouselContainer.style.backgroundImage = `url(./assets/carousel/${currentImageIndex}.jpg)`;
-    currentImageIndex++;
-    if (currentImageIndex > 3) {
-        currentImageIndex = 1;
-    }
+  carouselContainer.style.backgroundImage = `url(./assets/carousel/${currentImageIndex}.jpg)`;
+  currentImageIndex++;
+  if (currentImageIndex > 3) {
+    currentImageIndex = 1;
+  }
 }
 
 setInterval(carousel, 5000);
@@ -36,7 +36,7 @@ window.addEventListener("scroll", function () {
   if (scrollTop === 0) {
     secondHeader.classList.remove('bgColorWhenMove')
   }
-  
+
   lastScrollTop = scrollTop;
 });
 
@@ -69,4 +69,3 @@ function sliderMove() {
 }
 
 sliderMove();
-
